cdbd9b3122fdba49ff597bf548f16a15a64196c5
rassulmurat <rasusl1995@gmail.com>
2016-10-21 15:20:07 +0300

    keyborad_inter.c now reads key number

--- keyboard_inter.c
+++ keyboard_inter.c
@@ -3,9 +3,20 @@
 #include <linux/sched.h>
 #include <linux/interrupt.h>	
 
+#define KBD_IRQ             1       /* IRQ number for keyboard (i8042) */
+#define KBD_DATA_REG        0x60    /* I/O port for keyboard data */
+#define KBD_SCANCODE_MASK   0x7f
+#define KBD_STATUS_MASK     0x80
+
 static irqreturn_t keyboard_handler(int irq, void *dev_id)
 {
-	printk("Hello\n");
+	char scancode;
+
+    scancode = inb(KBD_DATA_REG);
+    /* NOTE: i/o ops take a lot of time thus must be avoided in HW ISRs */
+    printk("%x \n", scancode);
+
+    return IRQ_HANDLED;
 }
 
 static int __init keyboard_init(void)
